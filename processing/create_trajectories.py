b0419a9474ec0fe04eeb79782d811fe553df06d1
marcuschen92 <marcuschen@atomicmail.io>
2026-03-11 11:45:53 -0700

    Improve readability

--- processing/create_trajectories.py
+++ processing/create_trajectories.py
@@ -17,8 +17,28 @@ _note_claims_not_misinfo = pl.col("classification") == "NOT_MISLEADING"
 _ever_crh = pl.col("noteEverCrh")
 _never_crh = ~pl.col("noteEverCrh")
 
+_top_5_topics = ["sports", "diaries_&_daily_life", "business_&_entrepreneurs", "science_&_technology", "news_&_social_concern"]
 
-def _enrich_with_intercepts_and_factors(scored_notes: pl.DataFrame) -> pl.DataFrame:
+
+
+# Calculate calendar-based user month (months since first action) and calendar month
+def _enrich_with_user_and_calendar_month(df: pl.DataFrame) -> pl.DataFrame:
+    return df.with_columns(
+        _actionDt=pl.from_epoch(pl.col("createdAtMillis"), time_unit="ms"),
+        _firstActionDt=pl.from_epoch(pl.col("participantFirstActionMillis"), time_unit="ms"),
+    ).with_columns(
+        userMonth=(
+            (pl.col("_actionDt").dt.year() - pl.col("_firstActionDt").dt.year()) * 12
+            + pl.col("_actionDt").dt.month() - pl.col("_firstActionDt").dt.month()
+        ).cast(pl.Int32),
+        calendarMonth=pl.col("_actionDt").dt.strftime("%Y-%m"),
+    ).drop("_actionDt", "_firstActionDt")
+
+
+def _enrich_with_scores(
+    notes: pl.DataFrame, ratings: pl.DataFrame,
+) -> tuple[pl.DataFrame, pl.DataFrame]:
+    scores      = pl.read_parquet("data/2026-02-03-scored_notes.parquet")
     I_AND_F_COLUMNS = {
         "CoreModel (v1.1)": ("coreNoteIntercept", "coreNoteFactor1"),
         "ExpansionModel (v1.1)": ("expansionNoteIntercept", "expansionNoteFactor1"),
@@ -46,8 +66,8 @@ def _enrich_with_intercepts_and_factors(scored_notes: pl.DataFrame) -> pl.DataFr
         "InsufficientExplanation (v1.0)": (None, None),
     }
 
-    scored_notes = (
-        scored_notes
+    scores = (
+        scores
         .with_columns(scoreCreatedAtDt=pl.from_epoch(pl.col("createdAtMillis"), time_unit="ms"))
         # Infer the pre drift guard model
         .with_columns(
@@ -74,26 +94,7 @@ def _enrich_with_intercepts_and_factors(scored_notes: pl.DataFrame) -> pl.DataFr
         .rename({"finalRatingStatus": "noteFinalRatingStatus"})
         .select("noteId", "noteFinalRatingStatus", "numRatings", "decidedBy", "noteFinalIntercept", "noteFinalFactor")
     )
-    return scored_notes
 
-
-# Calculate calendar-based user month (months since first action) and calendar month
-def _enrich_with_user_and_calendar_month(df: pl.DataFrame) -> pl.DataFrame:
-    return df.with_columns(
-        _actionDt=pl.from_epoch(pl.col("createdAtMillis"), time_unit="ms"),
-        _firstActionDt=pl.from_epoch(pl.col("participantFirstActionMillis"), time_unit="ms"),
-    ).with_columns(
-        userMonth=(
-            (pl.col("_actionDt").dt.year() - pl.col("_firstActionDt").dt.year()) * 12
-            + pl.col("_actionDt").dt.month() - pl.col("_firstActionDt").dt.month()
-        ).cast(pl.Int32),
-        calendarMonth=pl.col("_actionDt").dt.strftime("%Y-%m"),
-    ).drop("_actionDt", "_firstActionDt")
-
-
-def _enrich_with_scores(
-    notes: pl.DataFrame, ratings: pl.DataFrame, scores: pl.DataFrame
-) -> tuple[pl.DataFrame, pl.DataFrame]:
     notes = notes.join(scores, on="noteId", how="left", coalesce=True, validate="1:1")
     ratings = ratings.join(scores, on="noteId", how="left", coalesce=True, validate="m:1")
     logger.info("Enriched notes and ratings with scores and factors")
@@ -102,8 +103,27 @@ def _enrich_with_scores(
 
 def _enrich_with_crh(
     notes: pl.DataFrame, ratings: pl.DataFrame, requests: pl.DataFrame,
-    note_ever_crh: pl.DataFrame, post_ever_crh: pl.DataFrame,
 ) -> tuple[pl.DataFrame, pl.DataFrame, pl.DataFrame]:
+    statuses    = pl.read_csv    ("data/2026-02-27-note_status_records.csv")    # Processed statuses, taken from a scm-prep run on 2/27.
+    # Calculate whether a note ever achieved CRH status
+    note_ever_crh = (
+        statuses
+        .with_columns(status_time=pl.col("status_time").str.to_datetime("%Y-%m-%dT%H:%M:%S%.f%z"))
+        .filter(pl.col("status_time") <= pl.datetime(2026, 2, 3, time_zone="UTC"))
+        .with_columns(Crh = pl.col("status") == "CURRENTLY_RATED_HELPFUL")
+        .group_by("note_id")
+        .agg(noteEverCrh = pl.col("Crh").any())
+    )
+    post_ever_crh = (
+        notes
+        .select("noteId", "tweetId")
+        .join(note_ever_crh, left_on="noteId", right_on="note_id", how="left", validate="1:1")
+        .group_by("tweetId")
+        .agg(postEverCrh=pl.col("noteEverCrh").fill_null(False).any())
+    )
+    logger.info(f"Calculated CRH statuses for {len(note_ever_crh):,} notes")
+    logger.info(f"Calculated CRH statuses for {len(post_ever_crh):,} posts")
+
     notes    = notes   .join(note_ever_crh, left_on="noteId", right_on="note_id", how="left", coalesce=True, validate="1:1")
     ratings  = ratings .join(note_ever_crh, left_on="noteId", right_on="note_id", how="left", coalesce=True, validate="m:1")
     requests = requests.join(post_ever_crh, on="tweetId", how="left", validate="m:1")
@@ -112,8 +132,13 @@ def _enrich_with_crh(
 
 
 def _enrich_with_topics(
-    notes: pl.DataFrame, ratings: pl.DataFrame, topics: pl.DataFrame,
+    notes: pl.DataFrame, ratings: pl.DataFrame,
 ) -> tuple[pl.DataFrame, pl.DataFrame]:
+    # Join anything outside to top 5 topics into "other"
+    topics      = pl.read_parquet("data/from-soham-notes_full.parquet")
+    topics = topics.with_columns(condensed_topic=pl.when(pl.col("topic").is_in(_top_5_topics)).then(pl.col("topic")).otherwise(pl.lit("other")))
+    topics = topics.select("noteId", "topic", "condensed_topic")
+
     notes   = notes  .join(topics, on="noteId", how="left", validate="1:1")  # TODO: Get more recent data from soham
     ratings = ratings.join(topics, on="noteId", how="left", validate="m:1")
     logger.info("Enriched notes and ratings with topics")
@@ -123,18 +148,33 @@ def _enrich_with_topics(
 
 def _enrich_with_first_action(
     notes: pl.DataFrame, ratings: pl.DataFrame, requests: pl.DataFrame,
-    first_action: pl.DataFrame,
-) -> tuple[pl.DataFrame, pl.DataFrame, pl.DataFrame]:
+) -> tuple[pl.DataFrame, pl.DataFrame, pl.DataFrame, pl.DataFrame]:
+    first_note_written      = notes     .group_by("noteAuthorParticipantId").agg(createdAtMillis=pl.col("createdAtMillis").min())
+    first_note_rated        = ratings   .group_by("raterParticipantId")     .agg(createdAtMillis=pl.col("createdAtMillis").min())
+    first_note_requested    = requests  .group_by("requesterParticipantId") .agg(createdAtMillis=pl.col("createdAtMillis").min())
+    first_action = (
+        pl.concat([
+            first_note_written  .rename({"noteAuthorParticipantId": "participantId"}),
+            first_note_rated    .rename({"raterParticipantId":      "participantId"}),
+            first_note_requested.rename({"requesterParticipantId":  "participantId"}),
+        ])
+        .group_by("participantId")
+        .agg(participantFirstActionMillis=pl.col("createdAtMillis").min())
+    )
+    logger.info(f"First action calculated for {len(first_action):,} users")
+
+
     notes    = notes   .join(first_action.rename({"participantId": "noteAuthorParticipantId"}),on="noteAuthorParticipantId",   how="left", validate="m:1")
     ratings  = ratings .join(first_action.rename({"participantId": "raterParticipantId"}),     on="raterParticipantId",        how="left", validate="m:1")
     requests = requests.join(first_action.rename({"participantId": "requesterParticipantId"}), on="requesterParticipantId",    how="left", validate="m:1")
     logger.info("Enriched notes and ratings with user join dates")
-    return notes, ratings, requests
+    return notes, ratings, requests, first_action
 
 
 def _enrich_with_partisanship(
-    notes: pl.DataFrame, ratings: pl.DataFrame, partisanship: pl.DataFrame,
+    notes: pl.DataFrame, ratings: pl.DataFrame,
 ) -> tuple[pl.DataFrame, pl.DataFrame]:
+    partisanship= pl.read_csv("data/renault_partisanship_labels.csv") # Partisanship data is from paper: "Republicans are flagged more often than Democrats for sharing misinformation on X's Community Notes" by Renault et al.
     party_cols = partisanship.select("note_id", "party").rename({"party": "postAuthorParty"})
     notes   = notes  .join(party_cols, left_on="noteId", right_on="note_id", coalesce=True, how="left", validate="1:1")
     ratings = ratings.join(party_cols, left_on="noteId", right_on="note_id", coalesce=True, how="left", validate="m:1")
@@ -180,68 +220,23 @@ if __name__ == "__main__":
     notes       = pl.read_parquet("data/2026-02-03/notes.parquet")
     ratings     = pl.read_parquet("data/2026-02-03/noteRatings.parquet")
     requests    = pl.read_parquet("data/2026-01-09/noteRequests.parquet").rename({"userId": "requesterParticipantId"}) # Using the user-level requests, not post-level requests!
-    statuses    = pl.read_csv    ("data/2026-02-27-note_status_records.csv")    # Processed statutes, taken from a scm-prep run on 2/27.
-    partisanship= pl.read_csv("data/renault_partisanship_labels.csv") # Partisanship data is from paper: "Republicans are flagged more often than Democrats for sharing misinformation on X's Community Notes" by Renault et al.
-    scores      = pl.read_parquet("data/2026-02-03-scored_notes.parquet")
-    scores      = _enrich_with_intercepts_and_factors(scores)
-    topics      = pl.read_parquet("data/from-soham-notes_full.parquet")
     logger.info("Data loaded successfully")
-
-    # Enrollment data not in users file; find the date each user made their first action
-    first_note_written      = notes     .group_by("noteAuthorParticipantId").agg(createdAtMillis=pl.col("createdAtMillis").min())
-    first_note_rated        = ratings   .group_by("raterParticipantId")     .agg(createdAtMillis=pl.col("createdAtMillis").min())
-    first_note_requested    = requests  .group_by("requesterParticipantId") .agg(createdAtMillis=pl.col("createdAtMillis").min())
-    first_action = (
-        pl.concat([
-            first_note_written  .rename({"noteAuthorParticipantId": "participantId"}),
-            first_note_rated    .rename({"raterParticipantId":      "participantId"}),
-            first_note_requested.rename({"requesterParticipantId":  "participantId"}),
-        ])
-        .group_by("participantId")
-        .agg(participantFirstActionMillis=pl.col("createdAtMillis").min())
-    )
-    logger.info(f"First action calculated for {len(first_action):,} users")
-
-    # Join anything outside to top 5 topics into "other"
-    top_5_topics = ["sports", "diaries_&_daily_life", "business_&_entrepreneurs", "science_&_technology", "news_&_social_concern"]
-    topics = topics.with_columns(condensed_topic=pl.when(pl.col("topic").is_in(top_5_topics)).then(pl.col("topic")).otherwise(pl.lit("other")))
-    topics = topics.select("noteId", "topic", "condensed_topic")
-    logger.info("Topics processed and enriched with condensed topic")
-
-    # Calculate whether a note ever achieved CRH status
-    note_ever_crh = (
-        statuses
-        .with_columns(status_time=pl.col("status_time").str.to_datetime("%Y-%m-%dT%H:%M:%S%.f%z"))
-        .filter(pl.col("status_time") <= pl.datetime(2026, 2, 3, time_zone="UTC"))
-        .with_columns(Crh = pl.col("status") == "CURRENTLY_RATED_HELPFUL")
-        .group_by("note_id")
-        .agg(noteEverCrh = pl.col("Crh").any())
-    )
-    post_ever_crh = (
-        notes
-        .select("noteId", "tweetId")
-        .join(note_ever_crh, left_on="noteId", right_on="note_id", how="left", validate="1:1")
-        .group_by("tweetId")
-        .agg(postEverCrh=pl.col("noteEverCrh").fill_null(False).any())
-    )
-    logger.info(f"Calculated CRH statuses for {len(note_ever_crh):,} notes")
-    logger.info(f"Calculated CRH statuses for {len(post_ever_crh):,} posts")
+    
+    ratings  = ratings .with_columns(ratingDate= pl.from_epoch(pl.col("createdAtMillis"), time_unit="ms").dt.date())
+    requests = requests.with_columns(requestDate=pl.from_epoch(pl.col("createdAtMillis"), time_unit="ms").dt.date())
 
     # Enrich
-    notes, ratings = _enrich_with_scores(notes, ratings, scores)
-    notes, ratings, requests = _enrich_with_crh(notes, ratings, requests, note_ever_crh, post_ever_crh)
-    notes, ratings = _enrich_with_topics(notes, ratings, topics)
-    notes, ratings, requests = _enrich_with_first_action(notes, ratings, requests, first_action)
+    notes, ratings = _enrich_with_scores(notes, ratings)
+    notes, ratings, requests = _enrich_with_crh(notes, ratings, requests)
+    notes, ratings = _enrich_with_topics(notes, ratings)
+    notes, ratings, requests, first_action = _enrich_with_first_action(notes, ratings, requests)
 
     notes    = _enrich_with_user_and_calendar_month(notes)
     ratings  = _enrich_with_user_and_calendar_month(ratings)
     requests = _enrich_with_user_and_calendar_month(requests)
-
-    ratings  = ratings   .with_columns(ratingDate= pl.from_epoch(pl.col("createdAtMillis"), time_unit="ms").dt.date())
-    requests = requests  .with_columns(requestDate=pl.from_epoch(pl.col("createdAtMillis"), time_unit="ms").dt.date())
     logger.info("Calculated user months and calendar months")
 
-    notes, ratings = _enrich_with_partisanship(notes, ratings, partisanship)
+    notes, ratings = _enrich_with_partisanship(notes, ratings)
     ratings = _enrich_ratings_with_note_data(ratings, notes)
     requests = _enrich_requests_with_outcomes(requests, notes)
 
@@ -260,7 +255,7 @@ if __name__ == "__main__":
             .filter(pl.col("condensed_topic") == topic)
             .count()
             .alias(f"{topic}Count")
-            for topic in top_5_topics + ["other"]
+            for topic in _top_5_topics + ["other"]
         ]
     ).sort("noteAuthorParticipantId", "userMonth")
     logger.info(f"Aggregated user notes: {len(user_notes):,} rows")
@@ -310,7 +305,7 @@ if __name__ == "__main__":
             .filter(pl.col("condensed_topic") == topic)
             .count()
             .alias(f"{topic}RatedCount")
-            for topic in top_5_topics + ["other"]
+            for topic in _top_5_topics + ["other"]
         ],
     ).with_columns(
         overallAccuracy=(pl.col("correctHelpfuls") + pl.col("correctNotHelpfuls")) / pl.col("notesRated"),
